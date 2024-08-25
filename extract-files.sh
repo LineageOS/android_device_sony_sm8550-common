3c0d85adfdb8692cbe54daa9ad9ca509bd44f7be
Michael Bestas <mkbestas@lineageos.org>
2024-08-25 21:46:46 +0300

    sm8550-common: Patch libwfdservice to resolve duplicate dependencies
    Fixes:
    error: module "libwfdservice" variant
    "android_arm_armv8-a_cortex-a76_static": depends on multiple
    versions of the same aidl_interface:
    android.media.audio.common.types-V2-cpp-source,
    android.media.audio.common.types-V3-cpp-source
    
    Change-Id: I8086a80fcc238d6b03d16640ee2c4e134129bbab

--- extract-files.sh
+++ extract-files.sh
@@ -91,6 +91,10 @@ function blob_fixup() {
             [ "$2" = "" ] && return 0
             sed -i "s/android.media.audio.common.types-V2-cpp.so/android.media.audio.common.types-V3-cpp.so/" "${2}"
             ;;
+        system_ext/lib64/libwfdservice.so)
+            [ "$2" = "" ] && return 0
+            sed -i "s/android.media.audio.common.types-V2-cpp.so/android.media.audio.common.types-V3-cpp.so/" "${2}"
+            ;;
         *)
             return 1
             ;;
