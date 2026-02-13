7a402600e7b25011a04454bae8e791e6e40355cd
Luofan Chen <clfbbn@gmail.com>
2026-02-13 12:08:11 +0800

    sm8550-common: Override BLE bit depth to 24-bit
    Change-Id: I2a80ff5b4de38f126678d02f2b22fd1c9e3d7039

--- common.mk
+++ common.mk
@@ -92,6 +92,7 @@ PRODUCT_PACKAGES += \
     android.hardware.authsecret@1.0.vendor
 
 # Bluetooth
+$(call soong_config_set,bluetooth_module,le_audio_bit_override,24)
 PRODUCT_PACKAGES += \
     lib_bt_aptx \
     lib_bt_ble \
